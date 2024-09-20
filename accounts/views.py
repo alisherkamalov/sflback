1f274c20255f8c5d5aa804333d4395d4c8266bdc
Alisher <161970644+alisherkamalov@users.noreply.github.com>
2024-09-21 00:24:35 +0500

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -16,6 +16,9 @@ class AccountView(APIView):
     def post(self, request):
         serializer = AccountSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            try:
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            except Exception as e:
+                return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
