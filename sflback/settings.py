188028b31f2bb1345a8e886026f10ac3da491967
Alisher <161970644+alisherkamalov@users.noreply.github.com>
2024-09-21 16:21:52 +0500

    Update settings.py

--- sflback/settings.py
+++ sflback/settings.py
@@ -97,10 +97,15 @@ WSGI_APPLICATION = 'sflback.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'steambase',
+        'USER': 'steamuser',
+        'PASSWORD': 'sflback8190',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
